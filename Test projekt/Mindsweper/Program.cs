905f57c4ae6208a3f473f7142a93584c6b1c4c58
Elias <32110106+lolmanQ@users.noreply.github.com>
2019-09-09 13:22:21 +0200

    created

---
+++ Test projekt/Mindsweper/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mindsweper
+{
+	class Program
+	{
+		public int[,] boombSheet = new int[,] { };
+		static void Main(string[] args)
+		{
+			
+
+		}
+	}
+}
