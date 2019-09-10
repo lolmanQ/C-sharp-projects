bfe4821604d2aae10c1433767aca2795b706d523
Elias <32110106+lolmanQ@users.noreply.github.com>
2019-09-10 14:09:58 +0200

    nothing

--- Test projekt/Mindsweper/Program.cs
+++ Test projekt/Mindsweper/Program.cs
@@ -11,7 +11,10 @@ namespace Mindsweper
 		public int[,] boombSheet = new int[,] { };
 		static void Main(string[] args)
 		{
-			
+			for(int i = 0; i < 10; i++)
+			{
+				Console.WriteLine(i);
+			}
 
 		}
 	}
